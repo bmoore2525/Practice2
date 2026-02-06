caec6761892a7ecb108ae2d16158c94a46ffee74
brad <brad@BRAD_PC_MAIN>
2026-02-06 12:10:40 -0500

    Add project files.

---
+++ Practice2/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+int total = 0;
+int[] numbers = { 1, 2, 3, 4, 5 };
+
+foreach(int num in numbers)
+{
+    Console.WriteLine(num);
+    total += num;
+}
+
+Console.WriteLine(total);
+
+Console.WriteLine();
+
+
+for(int i = numbers.Length; i >= 1; i--)
+{
+    Console.WriteLine(i);
+}
